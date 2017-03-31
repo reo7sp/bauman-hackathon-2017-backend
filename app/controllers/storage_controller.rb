4af051d9542a57b1e86dd2fcd7a8f1ee9d9dffb4
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-03-31 14:21:27 +0300

    File delete

--- app/controllers/storage_controller.rb
+++ app/controllers/storage_controller.rb
@@ -4,14 +4,21 @@ class StorageController < ApplicationController
   before_action :auth!
 
   def get
-    send_file user_path(params[:path]), filename: File.basename(params[:path])
+    send_file user_path, filename: File.basename(params[:path])
   rescue ActionController::MissingFile
     render json: { error: 'file not found' }, status: :not_found
   end
 
   def set
     FileUtils.mkdir_p(user_root)
-    File.binwrite(user_path(params[:path]), params[:data])
+    File.binwrite(user_path, params[:data])
+    render json: { ok: 1 }
+  rescue SystemCallError, IOError
+    render json: { error: 'io' }, status: :internal_server_error
+  end
+
+  def delete
+    File.delete(user_path)
     render json: { ok: 1 }
   rescue SystemCallError, IOError
     render json: { error: 'io' }, status: :internal_server_error
@@ -23,7 +30,7 @@ class StorageController < ApplicationController
     File.join(Rails.configuration.x.storage_root, Digest::MD5.hexdigest(@user.email))
   end
 
-  def user_path(path)
+  def user_path(path = params[:path])
     File.join(user_root, Digest::MD5.hexdigest(path))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post 'auth/register_device'
   post 'storage/get'
   post 'storage/set'
+  post 'storage/delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
